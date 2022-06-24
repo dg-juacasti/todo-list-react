2cda5ac5f72672dcf3c3cb17ed748f5fb7c7e5d8
MORALES DURAN SEBASTIAN ALEJANDRO <semorale@pichincha.com>
2022-06-24 13:21:11 -0500

    chapter quiz

--- src/components/molecules/todo/index.tsx
+++ src/components/molecules/todo/index.tsx
@@ -1,35 +1,45 @@
-import { FC } from "react"
-import { ITodoResponse } from "../../../models"
-import { COLORS } from "../../../shared/theme/colors"
-import { IconButton } from "../../atoms/icon-button"
-import Typography from "../../atoms/typography"
-import './index.scss'
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import { useList } from "../../../hooks/useLists";
+import { ITodoResponse } from "../../../models";
+import { COLORS } from "../../../shared/theme/colors";
+import { IconButton } from "../../atoms/icon-button";
+import Typography from "../../atoms/typography";
+import "./index.scss";
 export interface TodoProps {
-  todo: ITodoResponse
-  isEven: boolean
-  toggleComplete?(todo: ITodoResponse): void
-  deleteTodo?(todo: ITodoResponse): void
+  todo: ITodoResponse;
+  isEven: boolean;
+  toggleComplete?(todo: ITodoResponse): void;
+  deleteTodo?(todo: ITodoResponse): void;
 }
 
-export const Todo: FC<TodoProps> = ({ todo, isEven, toggleComplete = () => {} , deleteTodo = () => {} }) => {
-
+export const Todo: FC<TodoProps> = ({
+  todo,
+  isEven,
+  toggleComplete = () => {},
+}) => {
+  const { deleteToDo, editToDo } = useList();
   return (
-    <div className={`todo-wrapper todo-wrapper-${isEven ? 'even' : 'odd'}`}>
+    <div className={`todo-wrapper todo-wrapper-${isEven ? "even" : "odd"}`}>
       <div className={`todo-wrapper-element`}>
-        <input type="checkbox"/>
+        <input type="checkbox" />
         <div className={`todo-wrapper-information`}>
-          <Typography color={COLORS.textColor}>
-            {todo.description}
-          </Typography>
-          <Typography color={COLORS.textColor2}>
-            {todo.finish_at}
-          </Typography>
+          <Typography color={COLORS.textColor}>{todo.description}</Typography>
+          <Typography color={COLORS.textColor2}>{todo.finish_at}</Typography>
         </div>
       </div>
       <div className={`todo-wrapper-information`}>
-        <IconButton className="fa-solid fa-pencil"></IconButton>
-        <IconButton className="fa-solid fa-trash-can" ></IconButton>
+        <IconButton
+          className="fa-solid fa-pencil"
+          onClick={() =>
+            (window.location.href = `/create?id=${todo.id}&desc=${todo.description}&date=${todo.finish_at}`)
+          }
+        ></IconButton>
+        <IconButton
+          className="fa-solid fa-trash-can"
+          onClick={() => deleteToDo(todo.id!)}
+        ></IconButton>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/hooks/useLists.tsx
+++ src/hooks/useLists.tsx
@@ -1,24 +1,40 @@
-
-import { ITodoResponse } from "../models"
-import axios from 'axios'
-import { useState } from "react"
-import { AUTHOR_ID, BASE_URL } from "../constants/app"
+import { ITodoResponse } from "../models";
+import axios from "axios";
+import { useState } from "react";
+import { AUTHOR_ID, BASE_URL } from "../constants/app";
 
 export const useList = () => {
-
-  const [todos, setTodos] = useState<ITodoResponse[]>([])
+  const [todos, setTodos] = useState<ITodoResponse[]>([]);
 
   const refetch = () => {
-    axios.get(`${BASE_URL}?id_author=${AUTHOR_ID}`)
-      .then(res => {
-        const data = res.data
-        setTodos((data as any).data.map((todo: ITodoResponse): ITodoResponse => ({
-          id: todo.id,
-          status: todo.status,
-          description: todo.description,
-          finish_at: new Date(todo.finish_at).toISOString().slice(0, 10)
-        }))
-      )})
-  }
-  return { todos, refetch }
-}
+    axios.get(`${BASE_URL}?id_author=${AUTHOR_ID}`).then((res) => {
+      console.log(res);
+
+      const data = res.data;
+      setTodos(
+        (data as any).data.map(
+          (todo: ITodoResponse): ITodoResponse => ({
+            id: todo.id,
+            status: todo.status,
+            description: todo.description,
+            finish_at: new Date(todo.finish_at).toISOString().slice(0, 10),
+          })
+        )
+      );
+    });
+  };
+
+  const sendToDo = async (data: ITodoResponse) => {
+    await axios.post(`${BASE_URL}?id_author=${AUTHOR_ID}`, data);
+  };
+
+  const deleteToDo = async (taskid: number) => {
+    await axios.delete(`${BASE_URL}${taskid}`).then(() => refetch());
+  };
+
+  const editToDo = async (taskid: number, newData: ITodoResponse) => {
+    await axios.put(`${BASE_URL}${taskid}`, newData).then(() => refetch());
+  };
+
+  return { todos, refetch, sendToDo, deleteToDo, editToDo };
+};
